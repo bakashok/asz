90005b764925fb6d084f79a71d348c1c896ccc90
bakashok <ashok814249@gmail.com>
2020-07-14 19:41:19 +0530

    asz code challenge

---
+++ src/index.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import { Route, Switch, BrowserRouter } from "react-router-dom";
+import { Provider, connect } from "react-redux";
+import todos from "./components/todos/todos";
+import NotFound from "./components/notFound";
+import userTodo from "./reducers";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+
+import "./styles.css";
+import "antd/dist/antd.css";
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(userTodo, composeEnhancer(applyMiddleware(thunk)));
+
+class RootContainerComponent extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={todos} />
+          <Route component={NotFound} />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+}
+
+let RootContainer = connect()(RootContainerComponent);
+
+export default class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <RootContainer />
+      </Provider>
+    );
+  }
+}
+
+const rootElement = document.getElementById("asz");
+ReactDOM.render(<App />, rootElement);
